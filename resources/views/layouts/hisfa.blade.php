77a05042e4f9cd99f509b8b6e647abc9b8d7140a
Robrecht Vranckx <robrecht.vranckx@hotmail.com>
2016-10-05 18:58:44 +0200

    Revert "Revert "Login structure""
    This reverts commit 342d1e2cdf61328dc4f67f8b4d34d3acb3284d6c.

---
+++ resources/views/layouts/hisfa.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>OurApp</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <h1>Our App Logo</h1>
+        
+        @yield('content')
+        
+    </div>
+   
+    
+    @yield('footerscript')
+    
+</body>
+</html>
