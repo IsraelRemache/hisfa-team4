ee539a85c3feb12c8c099d24a4e429df9bc59fbd
MiguelIMD <r0579095@student.thomasmore.be>
2016-10-29 10:19:54 +0200

    De gebruiker kan kiezen welke mails hij ontvangt

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Auth;
+use Illuminate\Support\Facades\Input;
 use Image;
 use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
     public function index() {
-        return view('pages/profile', array('users'=> Auth::user()) );
+        $user = \Auth::user();
+
+        return view('pages/profile', array('user'=> $user ));
     }
 
     public function update(Request $request){
@@ -20,12 +23,27 @@ class UsersController extends Controller
         if($_FILES['picture']['size'] != 0){
 
             $request->picture->move(public_path('uploads'),"profile.jpg");
-            DB::table('users')->where('id',Auth::user()->id)->update(['img' => 'profile.jpg']);
+            $user = \Auth::user();
+            $user->img = 'profile.jpg';
+            $user->Save();
         }
         else{
-            return view('home');
+            //return view('home');
         }
 
+       if(isset($_POST['notifications']) === true){
+
+           $user = \Auth::user();
+           $user->notifications = true;
+           $user->Save();
+        }
+        else
+        {
+            $user = \Auth::user();
+            $user->notifications = false;
+            $user->Save();
+        }
+        return view('pages/profile', array('users'=> Auth::user()) );
     }
     
 }
