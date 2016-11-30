b78495e02c6324fcc590b05faa2f512d8205b252
MiguelIMD <r0579095@student.thomasmore.be>
2016-11-30 15:00:42 +0100

    css fix delete button + wheel in sidebar removed

--- resources/views/products/prime.blade.php
+++ resources/views/products/prime.blade.php
@@ -10,8 +10,17 @@ $primes = \App\Prime::all();
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{$prime->name}}</div>
-
+                    <div class="panel-heading" id="container_resource">
+                        <div><span class="name">{{$prime->name}}</span></div>
+                        <form action="{{ url("/primes/destroy")}}" method="post" enctype="multipart/form-data">
+                            <input type="hidden" value="{{csrf_token()}}" name="_token">
+                            <input type="hidden" value="{{$prime->id}}" name="id">
+                            <button type="submit" class="btn-delete" name="delete">
+                                <i class="material-icons">&#xE872;</i>
+                                Delete {{$prime->name}}
+                            </button>
+                        </form>
+                    </div>
                     <div class="panel-body">
                         <form id="profileform" class="form-horizontal" role="form" method="POST" action='{{ url("/primes/update")}}' enctype="multipart/form-data">
                             <div class="form-group">
@@ -26,7 +35,7 @@ $primes = \App\Prime::all();
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="name" class="col-md-4 control-label">Quantity</label>
+                                <label for="name" class="col-md-4 control-label">Quantity (in kg)</label>
                                 <div class="col-md-6">
                                     <input type="text" name="cprimesiloquantity" value="{{$prime->quantity}}" class="input form-control" style="border: none;">
                                 </div>
@@ -40,13 +49,6 @@ $primes = \App\Prime::all();
 
                                 </div>
                             </div>
-                        </form>
-                        <form action="{{ url("/primes/destroy")}}" method="post" enctype="multipart/form-data">
-                            <input type="hidden" value="{{csrf_token()}}" name="_token">
-                            <input type="hidden" value="{{$prime->id}}" name="id">
-                            <button type="submit" class="btn btn-primary" name="delete">
-                                Delete {{$prime->name}}
-                            </button>
                         </form>
                     </div>
                 </div>
